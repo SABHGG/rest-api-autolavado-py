ac5f13e0a89aee85026d27fd2728f5b3f9a4388b
sergiosabh05 <sergiosabh05@gmail.com>
2025-04-16 23:52:18 +0000

    update pemision to admin

--- app/routes/reports.py
+++ app/routes/reports.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.utils import require_auth, require_role
 from app.controllers.report_controller import ReportController
 
 reports_bp = Blueprint('reports', __name__, url_prefix='/reports')
 
 @reports_bp.route('/sales', methods=['GET'])
-@jwt_required()
+@require_auth()
+@require_role("admin")
 def generate_sales_report():
     current_user_id = get_jwt_identity()
     start_date = request.args.get('start_date')
